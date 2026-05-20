4daadb7819dc6525f2e44cd943705d78458ca517
ikafly144 <ikafly144@sabafly.net>
2026-05-20 15:02:29 +0900

    build: LDFLAGSから不要なオプションを削除

--- build.go
+++ build.go
@@ -2,7 +2,7 @@ package sdk
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/include
-#cgo !discord_partner_sdk LDFLAGS: -L. -ldiscord_partner_sdk
+#cgo !discord_partner_sdk LDFLAGS: -ldiscord_partner_sdk
 #cgo discord_partner_sdk LDFLAGS: -L${SRCDIR}/lib -ldiscord_partner_sdk
 */
 import "C"
